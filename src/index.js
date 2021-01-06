1a090a3cdeea1b71ff58cf43b00fe0c9c9c64361
Andres Ruiz <andrsruiz@gmail.com>
2021-01-06 13:16:51 -0600

    Add more boxes

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ import { gsap } from "gsap";
 import './style.css';
 
 function boxes(){
-	const element = document.createElement('div');
-	element.classList.add('box');
-	element.classList.add('green');
-	return element;
+	const boxOrange = document.createElement('div');
+	const boxGrey = document.createElement('div');
+	const boxGreen = document.createElement('div');
+
+	boxOrange.classList.add('box');
+	boxOrange.classList.add('orange');
+	boxGrey.classList.add('box');
+	boxGrey.classList.add('grey');
+	boxGreen.classList.add('box');
+	boxGreen.classList.add('green');
+
+	document.body.appendChild(boxOrange);
+	document.body.appendChild(boxGrey);
+	document.body.appendChild(boxGreen);
 }
 
-document.body.appendChild(boxes());
-
+boxes();
 gsap.to(".box", {duration: 2, x: 300});
 gsap.to(".green", {duration: 3, rotation: 360, scale: 0.5});
